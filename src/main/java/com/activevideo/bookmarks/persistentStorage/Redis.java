9683df2a8b84bcf9700073e51929dfbfae6c11ea
meerveld <meerveld@meerveld-2.local>
2012-09-14 13:35:42 +0200

    disabled redis for testing

--- src/main/java/com/activevideo/bookmarks/persistentStorage/Redis.java
+++ src/main/java/com/activevideo/bookmarks/persistentStorage/Redis.java
@@ -10,8 +10,8 @@ public class Redis {
    private Jedis jedis = null;
    
    protected Redis() {
-       jedis = new Jedis("127.0.0.1");
-       jedis.connect();
+       //jedis = new Jedis("127.0.0.1");
+       //jedis.connect();
        System.out.println("Redis Connected");
    }
    public static Redis getInstance() {
@@ -23,34 +23,36 @@ public class Redis {
    
    public String get(String clientId){
        System.out.println("get all: from clientid" + clientId);
-       Set<String> all = jedis.smembers("client:"+clientId);
+       /*Set<String> all = jedis.smembers("client:"+clientId);
        String response = "{\"response\": [";
        for(String bookmarkId : all) {
            response += "{\"bookmarkId\":\""+bookmarkId+"\",\"value\":"+jedis.get("bookmark:" + bookmarkId)+"},";
        }
        response = response.substring(0, response.length() - 1) + "]}";
-       return  response;
+       return  response;*/
+       return "{\"a\":\"a\"}";
    }
    
    public String get(String clientId, String bookmarkId){
        System.out.println("get: "+ bookmarkId);
        // todo filter clientId?
-       String bookmark = jedis.get("bookmark:" + bookmarkId);
+       //String bookmark = jedis.get("bookmark:" + bookmarkId);
+       String bookmark = "test";
        return "{\"response\":" + bookmark + "}";
    }
    
    public void set(String clientId, String bookmarkId, String value) {
        System.out.println("set: "+ bookmarkId + " with value " + value); 
-       jedis.sadd("client:"+clientId, bookmarkId);
-       jedis.set("bookmark:"+ bookmarkId, value);
+       //jedis.sadd("client:"+clientId, bookmarkId);
+       //jedis.set("bookmark:"+ bookmarkId, value);
    }
    
    public void delete(String clientId, String bookmarkId) {
        System.out.println("delete: "+ bookmarkId + " from client id "+ clientId);
-       jedis.srem("client:"+clientId, bookmarkId);
+       //jedis.srem("client:"+clientId, bookmarkId);
    }
    
    public void disconnect(){
-       jedis.disconnect();
+       //jedis.disconnect();
    }
 }
